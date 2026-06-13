28bc32bd652c50af2d214aebf2416565df9aae8f
Raghav <guptaraghav2608.com>
2026-06-13 17:49:29 +0530

    Code is Updated

--- Day12/Insert_Common_Divisor_In_LinkedList_Leetcode.cpp
+++ Day12/Insert_Common_Divisor_In_LinkedList_Leetcode.cpp
@@ -24,6 +24,15 @@ Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
+class ListNode {
+public:
+
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
 
 class Solution {
 public:
